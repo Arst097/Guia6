1f07816562503e025ed3fec80936609d04f29ff4
Sara Arevalo <arevaloc.sara@gmail.com>
2024-10-21 19:51:01 -0500

    Comentarios Ej4

--- src/MarcoTeorico/Ej4Hilos.java
+++ src/MarcoTeorico/Ej4Hilos.java
@@ -3,7 +3,7 @@ package MarcoTeorico;
 
 class HiloExtendThread extends Thread {
 
-    HiloExtendThread() {
+    HiloExtendThread() {                    //Constructor de la clase
         super("Hilo secundario");
         System.out.println("Hilo hijo: " + this);
         start();
@@ -14,6 +14,7 @@ class HiloExtendThread extends Thread {
             for (int i = 5; i > 0; i--) {
                 System.out.println("Hilo hijo: "+i);
                 Thread.sleep(500);
+                //La ejecución del hilo hijo sera cada medio segundo
             }
         } catch (InterruptedException e) {
             System.out.println("Interrupción del hilo hijo.");
@@ -26,16 +27,19 @@ class HiloExtendThread extends Thread {
 class ExtendThread {
     public static void main(String[] args) {
         
+        //Nombramiento del hilo principal
         Thread k = Thread.currentThread();
         k.setName("Hilo Principal");
-        System.out.println("Hilo padre: " + k);
+        System.out.println("Hilo padre: " + k);   
         
+        //Instancia de la clase que extiende a Thread
         new HiloExtendThread();
         
         try {
             for (int i = 5; i > 0; i--) {
                 System.out.println("Hilo padre: "+i);
-                Thread.sleep(1000);                
+                Thread.sleep(1000);
+                //Ejecución del hilo principal cada segundo
             }
         } catch (InterruptedException e) {
             System.out.println("Interrupcion del hilo padre");
